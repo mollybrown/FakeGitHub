85b56e8044312665df995402f418ae2530625273
Molly Brown <mollbrown@gmail.com>
2017-02-02 11:55:32 -0700

    add logout button and passing logout test

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get '/auth/github', as: :github_login
   get '/auth/github/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy', as: :github_logout
 
   resources :dashboard, only: [:index]
   resources :repositories, only: [:index]
--- spec/features/user_can_authenticate_their_github_account_spec.rb
+++ spec/features/user_can_authenticate_their_github_account_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 describe "Authentication Workflow:" do
+  before(:each) do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+      provider: 'github',
+      uid: '8581642',
+      credentials: {token: ENV['TOKEN']},
+      info: {nickname: 'mollybrown'}
+    })
+  end
 
   context "A user" do
     it "can visit the landing page (root)" do
@@ -14,22 +23,25 @@ describe "Authentication Workflow:" do
 
     it "can sign in with a existing GitHub account" do
       VCR.use_cassette(".login") do
-        OmniAuth.config.test_mode = true
-        OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
-          provider: 'github',
-          uid: '8581642',
-          credentials: {token: ENV['TOKEN']},
-          info: {nickname: 'mollybrown'}
-        })
 
         visit root_path
-
         click_link("Sign In")
 
         expect(current_path).to eq dashboard_index_path
         expect(page).to have_content("Molly Rachel Brown")
         expect(page).to have_content("mollybrown")
         expect(page).to have_content("mollyrachelbrown@gmail.com")
+      end
+    end
+
+    it "can sign out of their account" do
+      VCR.use_cassette(".login") do
+
+        visit root_path
+        click_link("Sign In")
+
+        expect(current_path).to eq dashboard_index_path
+        click_link("Sign Out")
       end
     end
   end
